35239819c4529d625c0b4d72ff09f2f82e431dde
Ryan Gildea <ryangildea@gmail.com>
2023-03-09 01:25:29 -0500

    lots

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,20 +1,28 @@
+import { Form, useActionData, useNavigation } from "@remix-run/react";
+
 import styles from "./NewNote.css";
 
 export default function NewNote() {
+  const data = useActionData();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <form method="post" id="note-form">
+    <Form method="post" id="note-form">
+      {data?.message && <p>{data.message}</p>}
       <p>
         <label htmlFor="note-title">Title</label>
-        <input type="text" name="note-title" id="note-title" />
+        <input type="text" name="title" id="title" />
       </p>
       <p>
         <label htmlFor="note-body">Content</label>
         <textarea id="content" name="content" rows="5" required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>Add Note</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,27 +1,74 @@
-import { redirect } from "@remix-run/node";
-import NewNote, { links as newNoteLinks } from "~/components/newNote";
-import { getStoredNotes } from "~/data/notes";
-import { storeNotes } from "../data/notes";
+import { json, redirect } from "@remix-run/node";
+import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
+import { storeNotes, getStoredNotes } from "~/data/notes";
+import { Link, useCatch, useLoaderData } from "@remix-run/react";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
+
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />"
     </main>
   );
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  if (!notes || notes.length === 0) {
+    throw json(
+      { message: "Could not find any notes." },
+      {
+        status: 404,
+        statusText: "Not Found",
+      }
+    );
+  }
+
+  return notes;
 }
 
 export async function action({ request }) {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
-  // Add validation...
+
+  if (noteData.title.trim().length < 5) {
+    return { message: "Title must be at least 5 characters long" };
+  }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = [...existingNotes, noteData];
   await storeNotes(updatedNotes);
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   return redirect("/notes");
 }
 
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function CatchBoundary({ error }) {
+  const caughtResponse = useCatch();
+  const message = caughtResponse.data?.message || "Data not found";
+
+  return (
+    <main>
+      <p className="info-message">{message}</p>
+    </main>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>{error.message}</p>
+      <p>
+        Back to <Link to="/">safety</Link>!
+      </p>
+    </main>
+  );
 }
